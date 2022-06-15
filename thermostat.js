33616243957c08045d4c4d1c17c409f0f65a1fd1
Adam <adam.j.mccor@gmail.com>
2022-06-15 17:26:20 +0100

    refactored thermostat.js

--- thermostat.js
+++ thermostat.js
@@ -1,9 +1,10 @@
 class Thermostat {
 
   constructor(temperature) {
+    this.DEFAULT_TEMPERATURE = 20;
     this.TEMPERATURE = 20;
     this.POWER_SAVE_MAX_TEMPERATURE = 24;
-    this.MAX_TEMPERATURE = 32;
+    this.MAX_TEMPERATURE = 31;
     this.MIN_TEMPERATURE = 10;
     this.POWER_SAVE = false;
 
@@ -14,9 +15,9 @@ class Thermostat {
   }
   
   up() {
-    if (this.POWER_SAVE === false && this.TEMPERATURE > 31) {
+    if (this.POWER_SAVE === false && this.TEMPERATURE > this.MAX_TEMPERATURE) {
       return;
-    } else if (this.POWER_SAVE === true && this.TEMPERATURE > this.MAX_TEMPERATURE) {
+    } else if (this.POWER_SAVE === true && this.TEMPERATURE > this.POWER_SAVE_MAX_TEMPERATURE) {
       return;
     } else {
     this.TEMPERATURE ++;
@@ -39,9 +40,18 @@ class Thermostat {
   }
 
   reset() {
-    this.TEMPERATURE = 20;
+    this.TEMPERATURE = this.DEFAULT_TEMPERATURE;
   }
 
+  checkEnergyUsage() {
+    if (this.TEMPERATURE < 18) {
+      return 'Low usage';
+    } else if (this.TEMPERATURE <= 25) {
+      return 'Medium usage';
+    } else {
+      return 'The temperature is TOO DAMN HIGHHHHHHHHHH';
+    }
+  }
 }
 
 module.exports = Thermostat;
